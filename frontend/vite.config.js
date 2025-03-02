d127c75e56ea5cc57049550d6450a65d5e0c9d7f
risshitachaturvedi <rishitachaturvedi@gmail.com>
2025-03-02 18:03:59 +0530

    hh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Put both plugins inside the array
+  plugins: [react(), tailwindcss()],
+  base: "/mini-map/", // Put both plugins inside the array
   server: {
     host: "0.0.0.0", // Expose to local network
     port: 5173, // Change if needed
